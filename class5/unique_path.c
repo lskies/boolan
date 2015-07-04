4f35937993e8f86cc5af08188fbc6ef16153c7d2
Fei Dong <fdong@coursera.org>
2015-07-04 15:49:34 +0800

    unique path

--- class5/unique_path.c
+++ class5/unique_path.c
@@ -1,10 +1,30 @@
-int dp(int m, int n) {
-    int mat[M_MAX+2][N_MAX+2] = {0};
-    mat[m][n+1] = 1;
+int uniquePathsBackTrack(int m, int n) {
+    if(m==1 || n==1) return 1;
+    return uniquePaths(m-1, n) + uniquePaths(m, n-1);
+}
 
-    for (int r = m; r >= 1; r--)
-        for (int c = n; c >= 1; c--)
-            mat[r][c] = mat[r+1][c] + mat[r][c+1];
-
-    return mat[1][1];
+int uniquePaths(int m, int n) 
+{
+    vector<vector<int> > table(m, vector<int>(n, 1));
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            table[i][j] = table[i-1][j] + table[i][j-1];
+        }
+    }
+    return table[m-1][n-1];
+}
+//Save space
+int uniquePaths2(int m, int n)
+{
+    vector<int> table(n, 1);
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            table[j] += table[j-1];
+        }
+    }
+    return table[n-1];
 }
