006f0c573fa2467c0c02dd0aee8f642efc5ceaa8
Fei Dong <fdong@coursera.org>
2015-07-04 14:57:45 +0800

    unique path

--- class5/unique_path.c
+++ class5/unique_path.c
@@ -1,9 +1,10 @@
-int uniquePaths(int m, int n) {
-    int ways[n] = {0};
-    
-    ways[n-1] = 1;
-    for(int i = m - 1; i >= 0; --i)
-        for(int j = n - 2; j >= 0; --j )
-            ways[j] += ways[j+1];
-    return ways[0];
+int dp(int m, int n) {
+    int mat[M_MAX+2][N_MAX+2] = {0};
+    mat[m][n+1] = 1;
+
+    for (int r = m; r >= 1; r--)
+        for (int c = n; c >= 1; c--)
+            mat[r][c] = mat[r+1][c] + mat[r][c+1];
+
+    return mat[1][1];
 }
